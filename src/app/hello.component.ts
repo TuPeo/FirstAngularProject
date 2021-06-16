ab09fa54070dcdbc88e47caae102ebcbb5a4ae47
TuPeo <nguyenthanhtu94@gmail.com>
2021-06-17 00:05:33 +0700

    day 3

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-hello",
-  template: "<h2>Hello world</h2>"
+  template: `
+    <h2>Hello world</h2>
+    <h3>Your name: {{user.name}}</h3>
+    <h3>Your age: {{user.age}}</h3>
+  `
 })
 
-export class HelloComponent {}
+export class HelloComponent {
+  user = {
+    name: "Admin",
+    age: 30
+  }
+}
